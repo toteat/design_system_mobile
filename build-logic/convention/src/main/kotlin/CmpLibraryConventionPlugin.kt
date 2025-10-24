26104487edecbd4da43720ae5dc98546a3ceeb04
luis-gonzalez-toteat <lgonzalez@toteat.com>
2025-10-23 21:17:29 -0300

    chore: re-run pipeline with write permissions

--- build-logic/convention/src/main/kotlin/CmpLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CmpLibraryConventionPlugin.kt
@@ -1,56 +1,81 @@
-// Tus imports existentes
+// CmpLibraryConventionPlugin.kt
 import com.toteat.designsystem.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.dependencies
-
-import org.gradle.api.publish.maven.MavenPublication // <-- IMPORTANTE
-import org.gradle.kotlin.dsl.withType                 // <-- IMPORTANTE
 import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
+
+import com.android.build.gradle.LibraryExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class CmpLibraryConventionPlugin: Plugin<Project> {
-    override fun apply(target: Project) {
-        with(target) {
+    override fun apply(target: Project) = with(target) {
 
+        with(pluginManager) {
+            // Asegúrate de tener estos plugins aquí (o en tu otro convention aplicado antes)
+            apply("org.jetbrains.kotlin.multiplatform")
+            apply("com.android.library")
+            apply("org.jetbrains.kotlin.plugin.compose")
+            apply("org.jetbrains.compose")
 
-            with(pluginManager) {
-                apply("com.toteat.convention.kmp.library")
-                apply("org.jetbrains.kotlin.plugin.compose")
-                apply("org.jetbrains.compose")
+            apply("maven-publish")
+        }
 
-                apply("maven-publish")
+        dependencies {
+            "commonMainImplementation"(libs.findLibrary("jetbrains-compose-ui").get())
+            "commonMainImplementation"(libs.findLibrary("jetbrains-compose-foundation").get())
+            "commonMainImplementation"(libs.findLibrary("jetbrains-compose-material3").get())
+            "commonMainImplementation"(libs.findLibrary("jetbrains-compose-material-icons-core").get())
+
+            "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
+        }
+
+        val libVersion = property("libVersion").toString() // p.ej. 0.1.1-SNAPSHOT
+        val libGroup   = property("libGroup").toString()   // p.ej. com.toteat.designsystem
+
+        extensions.configure<KotlinMultiplatformExtension> {
+            androidTarget {
+                publishLibraryVariants("debug", "release")
             }
 
-            dependencies {
-                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-ui").get())
-                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-foundation").get())
-                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-material3").get())
-                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-material-icons-core").get())
+            iosArm64()
+            iosX64()
+            iosSimulatorArm64()
+        }
 
-                "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
+
+        extensions.configure<LibraryExtension> {
+
+            if (namespace == null) {
+                namespace = "${libGroup}.toteatds"
             }
+            compileSdk = 34
+            defaultConfig { minSdk = 24 }
 
-            val libVersion = property("libVersion").toString()
-            val libGroup = property("libGroup").toString()
+            publishing {
+                singleVariant("debug")  { withSourcesJar() }
+                singleVariant("release") { withSourcesJar() }
+            }
+        }
 
 
-            extensions.configure<PublishingExtension> {
-                publications {
-                    withType<MavenPublication> {
-                        groupId = libGroup
-                        version = libVersion
-                    }
+        extensions.configure<PublishingExtension> {
+            publications {
+                withType<MavenPublication> {
+                    groupId = libGroup
+                    version = libVersion
                 }
-
-                repositories {
-                    maven {
-                        name = "GitHubPackages"
-                        url = uri("https://maven.pkg.github.com/toteat/design_system_mobile")
-                        credentials {
-                            username = System.getenv("GITHUB_ACTOR")
-                            password = System.getenv("GITHUB_TOKEN")
-                        }
+            }
+            repositories {
+                maven {
+                    name = "GitHubPackages"
+                    url = uri("https://maven.pkg.github.com/toteat/design_system_mobile")
+                    credentials {
+                        username = System.getenv("GITHUB_ACTOR") ?: findProperty("gpr.user")?.toString()
+                        password = System.getenv("GITHUB_TOKEN") ?: findProperty("gpr.key")?.toString()
                     }
                 }
             }
