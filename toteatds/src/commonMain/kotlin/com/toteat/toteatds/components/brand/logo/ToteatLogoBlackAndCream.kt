a3847cbc71bee6f9fe26747e8be1ffa8fda46fb8
nicobeltrami <nbeltrami@toteat.com>
2026-02-10 15:37:15 -0300

    feat: Add test tag support and content descriptions to logo components for improved accessibility and testing

--- toteatds/src/commonMain/kotlin/com/toteat/toteatds/components/brand/logo/ToteatLogoBlackAndCream.kt
+++ toteatds/src/commonMain/kotlin/com/toteat/toteatds/components/brand/logo/ToteatLogoBlackAndCream.kt
@@ -1,22 +1,29 @@
 package com.toteat.toteatds.components.brand.logo
-import com.toteat.toteatds.utils.setTestTag
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.toteat.toteatds.utils.setTestTag
 import designsystemmobile.toteatds.generated.resources.Res
 import designsystemmobile.toteatds.generated.resources.toteat_logo_black_and_cream
+import designsystemmobile.toteatds.generated.resources.toteat_logo_description
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ToteatLogoBlackAndCream(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    testTag: String = ""
 ) {
+    val description = contentDescription ?: stringResource(Res.string.toteat_logo_description)
+
     Image(
-        modifier = modifier,
+        modifier = modifier
+            .then(if (testTag.isNotEmpty()) Modifier.setTestTag(testTag) else Modifier),
         imageVector = vectorResource(Res.drawable.toteat_logo_black_and_cream),
-        contentDescription = null
+        contentDescription = description
     )
 }
 
--- toteatds/src/commonMain/kotlin/com/toteat/toteatds/components/brand/logo/ToteatLogoColor.kt
+++ toteatds/src/commonMain/kotlin/com/toteat/toteatds/components/brand/logo/ToteatLogoColor.kt
@@ -1,22 +1,29 @@
 package com.toteat.toteatds.components.brand.logo
-import com.toteat.toteatds.utils.setTestTag
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.toteat.toteatds.utils.setTestTag
 import designsystemmobile.toteatds.generated.resources.Res
 import designsystemmobile.toteatds.generated.resources.toteat_logo_color
+import designsystemmobile.toteatds.generated.resources.toteat_logo_description
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ToteatLogoOriginal(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    testTag: String = ""
 ) {
+    val description = contentDescription ?: stringResource(Res.string.toteat_logo_description)
+
     Image(
-        modifier = modifier,
+        modifier = modifier
+            .then(if (testTag.isNotEmpty()) Modifier.setTestTag(testTag) else Modifier),
         imageVector = vectorResource(Res.drawable.toteat_logo_color),
-        contentDescription = null
+        contentDescription = description
     )
 }
 
